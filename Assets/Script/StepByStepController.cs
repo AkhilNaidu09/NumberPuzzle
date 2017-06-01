555765e592e1ce056076f4cea50fa580713b5df0
Akhil Naidu <akil.naidu28@gmail.com>
2017-06-02 00:40:38 +0530

    Fix mute button issue

--- Assets/Script/StepByStepController.cs
+++ Assets/Script/StepByStepController.cs
@@ -24,6 +24,25 @@ public class StepByStepController : MonoBehaviour {
 		gameMN.col = col;
 		gameMN.row = row;
 		gameMN.startControl = true;
+		ToggleSound ();
+	}
 
+	void ToggleSound()
+	{
+		//Converting Mouse Pos to 2D (vector2) World Pos
+		Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		RaycastHit2D hit=Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+		if (hit)
+		{
+			if(hit.transform.name == "Volume")
+			{
+				if (gameMN.mute) {
+					gameMN.mute = false;
+				} else {
+					gameMN.mute = true;
+				}
+			}
+		}
 	}
 }
